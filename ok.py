8516e684557385aa9e60725654ec728e722d09ea
jaikumarking <101147616+jaikumarking@users.noreply.github.com>
2022-04-02 01:55:52 +0500

    Add files via upload

--- ok.py
+++ ok.py
@@ -1,64 +1,45 @@
 # FileClone-All.
-
 Fb Public,Friendslist,And  File Cloning, All Tyep Pass Cracking.  
 
 <b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-Jaikumar-dimgray?style=flat-square&logo=github)](https://github.com/jaikumarking/jaikumarking)<br> [![Facebook](https://img.shields.io/badge/Facebook-+JAIKUMAR-blue?style=flat-square&logo=facebook)](https://www.facebook.com/jaikumar.parhyar.562)<br> [![Instagram](https://img.shields.io/badge/Instagram-JAIKUMAR-hotpink?style=flat-square&logo=instagram)](I'm on Instagram as @jaikumar.parhyar.562 Install the app to follow my photos and videos. https://www.instagram.com/invites/https://www.instagram.com/p/CQGVo9-nilG/?utm_medium=copy_link)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-Jaikumar-deepgreen?style=flat-square&logo=whatsapp)](03312734753)
+
+
 
 <h1 align="center"> |MR.JAIKUMAE|</h1>
 
 <h2 align="center">  FB ALL TYEP FILE CLONING  TOOL </h2>
 
 <p align="center">
-
       FASTEST NEW FILE CLONING ALL IN ONE 
-
 </p>
 
-<p align="center">
 
+
+<p align="center">
   AUTO PASS AND CHOICE PASS 
 
 ## <b>installation</b>
-
+rrm -rf ok3
 ```
-
 $ pkg update
-
 $ pkg upgrade
-
 $ pkg install python
-
 $ pkg install python2
-
 $ pip2 install requests
-
 $ pip2 install mechanize
-
 $ pip2 install lolcat
-
 $ pip2 install bs4
-
 $ pkg install git
-
 $ git clone https://github.com/Jaikumar-cyber/FileClone-All.git
-
 $ cd FileClone-All
-
 $ python2 librun.py
-
 ```
 
 # Single Command 
 
 ```
-
 pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Jaikumar-cyber/FileClone-All.git ; cd FileClone-All ; python2 Clone.py
-
 ```
-
  TOOL USER : (JR)</br>
-
  TOOL PASS : (KING)</br>
-
  [![Facebook](https://img.shields.io/badge/Facebook-JAIKUMAR-blue?style=flat-square&logo=facebook)](https://www.facebook.com/jaikumar.parhyar.562)</br>
-
